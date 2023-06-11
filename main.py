626820d6a28a6a255d5f1cee09bfd05a4ca6f167
SIWAKORN SIRIPHAN <mekk.siriphan@gmail.com>
2023-06-11 11:42:14 +0700

    import

---
+++ main.py
@@ -0,0 +1,11 @@
+import csv
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import datetime #หาวันที่ปัจจุบันด้วย library datetime
+# Thai font
+!wget https://github.com/Phonbopit/sarabun-webfont/raw/master/fonts/thsarabunnew-webfont.ttf
+
+import matplotlib as mpl
+mpl.font_manager.fontManager.addfont('thsarabunnew-webfont.ttf') # 3.2+
+mpl.rc('font', family='TH Sarabun New')
